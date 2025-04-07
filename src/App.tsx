6262519aa5aed92495eb173da215fd3fad9804a0
DimitriGilbert <dimitri.gilbert@gmail.com>
2025-04-07 13:07:47 +0200

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,14 @@ const chatProviders: AiProviderConfig[] = [
     requiresApiKey: false,
     models: [
       {
-        id: "gemma-3-12b-it",
-        name: "gemma3 12b",
-        instance: lmstudio("gemma-3-12b-it"),
-      },
-      {
         id: "gemma-3-1b-it",
         name: "gemma3 1b",
         instance: lmstudio("gemma-3-1b-it"),
+      },
+      {
+        id: "gemma-3-12b-it",
+        name: "gemma3 12b",
+        instance: lmstudio("gemma-3-12b-it"),
       },
     ],
   },
